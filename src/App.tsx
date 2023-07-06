8bff6ce80b9972235da4015003f2ee16eb644c3b
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-06 16:51:13 +0100

    define main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 export default function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
