930c85da9c87ec4fa9a697d5441a23ee42b57c38
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-07 15:41:47 +0100

    handle loading state

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,10 +1,16 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
