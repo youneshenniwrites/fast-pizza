66cf6f78f5d3aaaece9485bcd3e094b06b568f5e
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-08 22:15:09 +0100

    fix styling issue

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import { ButtonProps } from "../types";
 
-const baseStyle: string =
+const baseStyle =
   "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
-const buttonStyles = {
+const buttonStyles: { [key: string]: string } = {
   primary: baseStyle + " px-4 py-3 md:px-6 md:py-4",
   small: baseStyle + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
   secondary:
@@ -12,17 +12,15 @@ const buttonStyles = {
 } as const;
 
 export default function Button({ children, disabled, to, type }: ButtonProps) {
-  const className = `${baseStyle} ${buttonStyles[type]}`;
-
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={buttonStyles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={buttonStyles[type]}>
       {children}
     </button>
   );
