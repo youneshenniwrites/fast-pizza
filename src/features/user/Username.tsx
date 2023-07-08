48c3db38341eef9a6d1f71fef8e9cbb47c260ab4
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-08 20:52:54 +0100

    fix a few compile issues

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  return <div className="hidden text-sm font-semibold md:block">Younes</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
