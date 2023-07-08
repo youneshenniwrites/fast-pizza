4bb501a9ef8bf8488467423e7eb9cac58d11439e
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-08 19:46:26 +0100

    create custom buttons

---
+++ src/ui/Button.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  type: string;
+  disabled?: boolean;
+  to?: string;
+};
+
+type ButtonType = { [key: string]: string };
+
+export default function Button({ children, disabled, to, type }: ButtonProps) {
+  const base =
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles: ButtonType = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
