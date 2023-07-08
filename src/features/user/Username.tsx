e13c01ee24f843a8b9e406cee730fa3db8f69d1b
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-08 21:33:39 +0100

    add redux and configure store and create usr slice

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,3 +1,11 @@
+import { useAppSelector } from "../../store/hooks";
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Younes</div>;
+  const username = useAppSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,4 +1,5 @@
-import { getAddress } from "../../services/apiGeocoding";
+import { createSlice } from "@reduxjs/toolkit";
+// import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition(): Promise<GeolocationPosition> {
   return new Promise(function (resolve, reject) {
@@ -6,18 +7,36 @@ function getPosition(): Promise<GeolocationPosition> {
   });
 }
 
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+// async function fetchAddress() {
+//   //* 1) We get the user's geolocation position
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+//   //* 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
+//   //* 3) Then we return an object with the data that we are interested in
+//   return { position, address };
+// }
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
