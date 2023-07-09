f4561c105ef59026eb07241fe9e8ae2e42ac7e5c
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-09 13:51:47 +0100

    remove fake cart data

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -5,30 +5,6 @@ import { useAppSelector } from "../../store/hooks";
 import { getCartItems } from "./cartSlice";
 import { getUsername } from "../user/userSlice";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function Cart() {
   const username = useAppSelector(getUsername);
   const cart = useAppSelector(getCartItems);
