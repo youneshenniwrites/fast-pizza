efb00efd6a382ac549a95e40092c6078a03fd4aa
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-09 17:27:42 +0100

    hide cart footer on cart page

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
 import { getCartTotalPrice, getCartTotalQuantity } from "./cartSlice";
-import { formatCurrency } from "../../utils/helpers";
+import { formatCurrency, useCurrentRoute } from "../../utils/helpers";
 
 export default function CartOverview() {
-  const totalCartQauntity = useAppSelector(getCartTotalQuantity);
+  const totalCartQuantity = useAppSelector(getCartTotalQuantity);
   const totalCartPrice = useAppSelector(getCartTotalPrice);
 
-  if (!totalCartQauntity) return null;
+  const currentRoute = useCurrentRoute();
+
+  if (!totalCartQuantity || currentRoute === "/cart") return null;
 
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>{totalCartQauntity} pizzas</span>
+        <span>{totalCartQuantity} pizzas</span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { useLocation } from "react-router-dom";
+
 export function formatCurrency(value: number | bigint) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -24,3 +26,9 @@ export const isValidPhone = (str: string) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
+
+// TODO ideally this custon hook should be moved to s hooks folder
+export function useCurrentRoute() {
+  const location = useLocation();
+  return location.pathname;
+}
