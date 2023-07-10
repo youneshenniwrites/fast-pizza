e35346cf0267bc17615563dbdfa9890ab77de6be
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-10 15:33:10 +0100

    create an async get address action using rtk thunk

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,35 +1,41 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../store/store";
-// import { getAddress } from "../../services/apiGeocoding";
+import { getAddress, getPosition } from "../../services/apiGeocoding";
 
-// function getPosition(): Promise<GeolocationPosition> {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+export const fetchAddress = createAsyncThunk(
+  "user/fatchAdress",
+  async function () {
+    //* 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-// async function fetchAddress() {
-//   //* 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
+    //* 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-//   //* 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-//   //* 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
+    //* 3) Then we return an object with the data that we are interested in
+    // * Note that this will be the payload of the fullfilled state
+    return { position, address };
+  }
+);
 
 type UserState = {
   username: string;
+  status: string;
+  position: object;
+  address: string;
+  error: string | undefined;
 };
 
 const initialState: UserState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -40,6 +46,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, _action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
--- src/services/apiGeocoding.ts
+++ src/services/apiGeocoding.ts
@@ -11,3 +11,9 @@ export async function getAddress({ latitude, longitude }: getAddressProps) {
   const data = await res.json();
   return data;
 }
+
+export function getPosition(): Promise<GeolocationPosition> {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
